eb8727c4f5ffbc2ba7aa94213023e3c9980537b9
Anda Andronie <anda.andronie@student.manchester.ac.uk>
2018-08-07 13:51:02 +0300

    pools app

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
+from dajngo.urls import include, path
 
 urlpatterns = [
    url(r'^admin/', admin.site.urls),
+   path('polls/', include('polls.urls')),
    url(r'^', include('helloworld.urls')), 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
